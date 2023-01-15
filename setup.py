e90138b49d54b5abe6db4b6185e605e366a8f21d
Borna Ahmadzadeh <borna.ahz@gmail.com>
2023-01-15 09:19:01 -0500

    Release 0.0.3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
 	name='flaim',
-	version='0.0.2',
+	version='0.0.3',
 	description='Flax Image Models',
 	long_description=open('README.md').read(),
 	long_description_content_type='text/markdown',
